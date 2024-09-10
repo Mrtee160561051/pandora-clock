cefef072d93e15c9e85145aa9bdc5d9917f86ce8
BiggyTech <akinyemitestimonyiadedayo@gmail.com>
2024-09-10 18:09:07 +0530

    change index chage path in index

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/PandoraClock/",
+  base:"/Pandora-clock/",
   plugins: [react()],
 })
